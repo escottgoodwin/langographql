b31ddf8c584780fd5b7af310317d83f6470293eb
Evan Goodwin <evangoodwin@Evans-MacBook-Pro.local>
2019-09-25 16:17:19 -0700

    art recs return

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,6 @@
 async function articleRecommendations(parent, args, context, info) {
   const { db, user } = context
+  console.log(user)
   const { lang } = args
   if (!user){
     throw error
@@ -7,13 +8,18 @@ async function articleRecommendations(parent, args, context, info) {
 
   query2 = `select recs from recommendations where uid = '${user.uid}' AND lang = '${lang}' ORDER BY rec_date DESC LIMIT 1`
   const results = await db.query(query2)
-  const art_ids = results.rows
-
-  query1 = `select * from ${lang}_arts where art_id in ${art_ids}`
+  if (results.rows.length>1){
+    const art_ids = results.rows[0]
+    console.log(art_ids)
+    query1 = `select * from ${lang}_arts where art_id in ${art_ids}`
   
-  query3 = `select * from ${lang}_arts where uid in (SELECT uid FROM recommendations WHERE uid like ${user.uid})`
-  const results1 = await db.query(query1)
-  return results1.rows
+    query3 = `select * from ${lang}_arts where uid in (SELECT uid FROM recommendations WHERE uid like ${user.uid})`
+    const results1 = await db.query(query1)
+    console.log(results1)
+    return results1.rows
+    } else { 
+      return []
+    }
 }
 
 async function article(parent, args, context, info) {

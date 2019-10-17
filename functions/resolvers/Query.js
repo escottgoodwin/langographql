739421f512b43c6f7f221979aef2a21ecf36d341
Evan Goodwin <evangoodwin@Evans-MacBook-Pro.local>
2019-10-16 17:46:05 -0700

    article recs

--- functions/resolvers/Query.js
+++ functions/resolvers/Query.js
@@ -1,25 +1,81 @@
+async function getCluster(db, lang, uid, cluster_num){
+  query = `SELECT art_id, link, title, dt FROM ${lang}_arts
+  WHERE art_id in
+  (SELECT art_id FROM recommendations
+  WHERE uid='${uid}' AND cluster_num=${cluster_num})`
+
+  const results = await db.query(query)
+  const recommendations = results.rows.map(r => ({art_id: r.art_id,  link: r.link, title: r.title, lang, date: r.dt}))
+
+  return recommendations
+}
+
+async function getRecs(db, lang, art_ids){
+
+  query = `SELECT art_id, link, title, dt FROM ${lang}_arts WHERE art_id = ANY($1)`
+
+  const results = await db.query(query,[art_ids])
+
+  const recs = results.rows.map(r => ({art_id: r.art_id,  link: r.link, title: r.title, lang, date: r.dt}))
+
+  return recs
+}
+
+function groupBy(arr, criteria) {
+  return arr.reduce(function (obj, item) {
+   var key = typeof criteria === 'function' ? criteria(item) : item[criteria];
+     if (!obj.hasOwnProperty(key)) { obj[key] = [] }
+     obj[key].push(item)
+     return obj;
+   }, {})
+ }
+
 async function articleRecommendations(parent, args, context, info) {
   const { db, user } = context
-  console.log(user)
+  const { uid } = user
+
   const { lang } = args
   if (!user){
     throw error
   }
-
-  query2 = `select recs from recommendations where uid = '${user.uid}' AND lang = '${lang}' ORDER BY rec_date DESC LIMIT 1`
-  const results = await db.query(query2)
-  if (results.rows.length>1){
-    const art_ids = results.rows[0]
-    console.log(art_ids)
-    query1 = `select * from ${lang}_arts where art_id in ${art_ids}`
   
-    query3 = `select * from ${lang}_arts where uid in (SELECT uid FROM recommendations WHERE uid like ${user.uid})`
-    const results1 = await db.query(query1)
-    console.log(results1)
-    return results1.rows
-    } else { 
-      return []
+  query = `SELECT art_id, cluster_num FROM recommendations WHERE uid='${uid}'`
+
+  const results = await db.query(query)
+  const recsraw = results.rows
+
+  const grouped = groupBy(recsraw, 'cluster_num')
+  const groupedIds = Object.values(grouped).map(g => g.map(a => a.art_id))
+  clusterRecs1 = groupedIds.map(async (ai) => {
+    const recs = await getRecs(db, lang, ai)
+    return {
+      recs: recs
     }
+  })
+  
+  const clusterRecs = await Promise.all(clusterRecs1)
+
+  return clusterRecs
+}
+
+async function articleRecommendations1(parent, args, context, info) {
+  const { db, user } = context
+  const { uid } = user
+
+  const { lang } = args
+  if (!user){
+    throw error
+  }
+  query = `SELECT art_id, link, title, dt FROM ${lang}_arts
+  WHERE art_id in
+  (SELECT art_id FROM recommendations
+  WHERE uid='${uid}')`
+
+  const results = await db.query(query)
+  const recommendations = results.rows.map(r => ({art_id: r.art_id,  link: r.link, title: r.title, lang, date: r.dt}))
+
+  return recommendations
+
 }
 
 async function article(parent, args, context, info) {
@@ -54,6 +110,7 @@ async function translations(parent, args, context, info) {
 
 module.exports = {
   articleRecommendations,
+  articleRecommendations1,
   article,
   translations
 }
